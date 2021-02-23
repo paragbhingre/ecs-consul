6870f18437a51263542d7477b9f7baea3bb7dc0a
Stanko <stankoa@amazon.com>
2021-02-22 17:33:13 -0800

    add IP list to startup.sh

--- mesh-templates/fargate/init/startup.sh
+++ mesh-templates/fargate/init/startup.sh
@@ -15,12 +15,15 @@ echo '{
     }
 }' >> ${CONSUL_DIR}/service-counting.json
 
+# Ideally we'd use {"retry_join": ["provider=aws tag_key=Name tag_value='$ENV_NAME'-consul-server"]}
+# ...instead of a hardcoded list of IPs, but can't yet. See: https://github.com/hashicorp/go-discover/issues/61
+
 echo "writing config file..."
 echo '{
     "node_name": "counting-client-1",
     "client_addr": "0.0.0.0",
     "data_dir": "/consul/data",
-    "retry_join": ["provider=aws tag_key=Name tag_value='$ENV_NAME'-consul-server"],
+    "retry_join": ["10.0.0.17","10.0.1.26","10.0.1.46"],
     "advertise_addr":' $ECS_IPV4 '
 }' >> ${CONSUL_DIR}/config.json
 
