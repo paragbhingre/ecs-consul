53040584dff3df6fbd927742fbb973e11f62d144
Stanko <stankoa@amazon.com>
2021-02-25 11:23:25 -0800

    update init to use AWS autojoin, DockerName as node name

--- fargate/init/startup.sh
+++ fargate/init/startup.sh
@@ -5,7 +5,9 @@ echo "discovered IPv4 address is: " $ECS_IPV4
 
 ENV_NAME="consulprod"
 CONSUL_DIR="/consul/config"
-UUID=$(curl -s $ECS_CONTAINER_METADATA_URI | jq '.Labels["com.amazonaws.ecs.task-arn"]')
+# TODO: use task ID (but not full ARN, b/c contains bad chars)
+# get from $(curl -s $ECS_CONTAINER_METADATA_URI | jq '.Labels["com.amazonaws.ecs.task-arn"]')
+UUID=$(curl -s $ECS_CONTAINER_METADATA_URI | jq '.DockerName') # 
 
 echo "writing service file..."
 echo '{
@@ -20,15 +22,15 @@ echo '{
     }
 }' >> ${CONSUL_DIR}/service-counting.json
 
-# Ideally we'd use {"retry_join": ["provider=aws tag_key=Name tag_value='$ENV_NAME'-consul-server"]}
-# ...instead of a hardcoded list of IPs, but can't yet. See: https://github.com/hashicorp/go-discover/issues/61
+# Currently need to specify region. See: https://github.com/hashicorp/go-discover/issues/61
+# old IP list: ["10.0.0.17","10.0.1.26","10.0.1.46"]
 
 echo "writing config file..."
 echo '{
     "node_name": '$UUID',
     "client_addr": "0.0.0.0",
     "data_dir": "/consul/data",
-    "retry_join": ["10.0.0.17","10.0.1.26","10.0.1.46"],
+    "retry_join": ["provider=aws region=us-west-1 tag_key=Name tag_value=consulprod-consul-server"],
     "advertise_addr":' $ECS_IPV4 '
 }' >> ${CONSUL_DIR}/config.json
 
